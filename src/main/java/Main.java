17124946a5df4e035f5f633c54f1526c6218b8a6
Name <dinur5@yandex.ru>
2023-03-16 09:13:08 +0700

    fixed bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String ...args) {
         int columnNumber = Integer.parseInt(args[0]) - 1;
 
-        CsvFileCli cli = new CsvFileCli("/airports.csv", 2);
+        CsvFileCli cli = new CsvFileCli("/airports.csv", columnNumber);
         cli.initialize();
         cli.start();
     }
